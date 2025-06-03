8f59fd2487739912ca38098b68b212877e7e2e83
airusk <ksuriawijaya@gmail.com>
2025-06-03 00:31:15 -0700

    Revises base in vite config

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -18,7 +18,7 @@ function App({children}) {
       <BrowserRouter>
         <NavigationBar/>
         <Routes>
-            <Route path = "/" element={<Home/>}/>
+            <Route path = "/home" element={<Home/>}/>
             <Route path = "/projects" element={<Projects/>}/>
             <Route path = "/about" element={<About/>}/>
         </Routes>
